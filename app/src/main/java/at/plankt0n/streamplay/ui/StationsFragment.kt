f3108b1fb176a010709749300e888fc884df5e3e
S. J. Plankton <97785891+Planqton@users.noreply.github.com>
2025-06-15 16:47:58 +0200

    Fix scrolling in station list and remove top bar

--- app/src/main/java/at/plankt0n/streamplay/ui/StationsFragment.kt
+++ app/src/main/java/at/plankt0n/streamplay/ui/StationsFragment.kt
@@ -8,8 +8,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
-import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
@@ -29,7 +27,6 @@ import at.plankt0n.streamplay.helper.PlaylistURLHelper
 import at.plankt0n.streamplay.helper.PreferencesHelper
 import at.plankt0n.streamplay.helper.MediaServiceController
 import at.plankt0n.streamplay.helper.StateHelper
-import at.plankt0n.streamplay.MainActivity
 import androidx.viewpager2.widget.ViewPager2
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
@@ -42,8 +39,6 @@ class StationsFragment : Fragment() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: StationListAdapter
     private lateinit var mediaServiceController: MediaServiceController
-    private lateinit var topbarBackButton: ImageButton
-    private lateinit var topbarTitle: TextView
 
     companion object {
         private const val REQUEST_CODE_IMPORT_JSON = 1001
@@ -55,8 +50,6 @@ class StationsFragment : Fragment() {
     ): View {
         val view = inflater.inflate(R.layout.fragment_stations, container, false)
 
-        topbarBackButton = view.findViewById(R.id.arrow_back)
-        topbarTitle = view.findViewById(R.id.topbar_title)
 
         stationList = PreferencesHelper.getStations(requireContext()).toMutableList()
 
@@ -143,8 +136,10 @@ class StationsFragment : Fragment() {
         val parentPager = parentFragment?.view?.findViewById<ViewPager2>(R.id.main_view_pager)
         recyclerView.setOnTouchListener { _, event ->
             when (event.actionMasked) {
-                MotionEvent.ACTION_DOWN -> parentPager?.requestDisallowInterceptTouchEvent(true)
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> parentPager?.requestDisallowInterceptTouchEvent(false)
+                MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE ->
+                    parentPager?.requestDisallowInterceptTouchEvent(true)
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL ->
+                    parentPager?.requestDisallowInterceptTouchEvent(false)
             }
             false
         }
@@ -171,10 +166,6 @@ class StationsFragment : Fragment() {
             startActivityForResult(intent, REQUEST_CODE_IMPORT_JSON)
         }
 
-        topbarBackButton.setOnClickListener {
-            refreshPlaylist()
-            (activity as? MainActivity)?.showPlayerPage()
-        }
 
         return view
     }
