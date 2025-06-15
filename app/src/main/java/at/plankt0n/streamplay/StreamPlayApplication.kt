c6268137164f168b1b4a88c0dad0581e9d8685bd
S. J. Plankton <97785891+Planqton@users.noreply.github.com>
2025-06-15 22:46:26 +0200

    Add crash logging handler

---
+++ app/src/main/java/at/plankt0n/streamplay/StreamPlayApplication.kt
@@ -0,0 +1,11 @@
+package at.plankt0n.streamplay
+
+import android.app.Application
+import at.plankt0n.streamplay.helper.CrashHandler
+
+class StreamPlayApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Thread.setDefaultUncaughtExceptionHandler(CrashHandler(this))
+    }
+}
