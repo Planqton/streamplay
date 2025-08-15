310a1b11562179ef5598c611618aefc3c96668a3
S. J. Plankton <97785891+Planqton@users.noreply.github.com>
2025-08-15 15:40:48 +0200

    Fix SVG listener signature

--- app/src/main/java/at/plankt0n/streamplay/glide/SvgSoftwareLayerSetter.kt
+++ app/src/main/java/at/plankt0n/streamplay/glide/SvgSoftwareLayerSetter.kt
@@ -2,7 +2,6 @@ package at.plankt0n.streamplay.glide
 
 import android.graphics.drawable.PictureDrawable
 import android.view.View
-import android.widget.ImageView
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
 import com.bumptech.glide.request.RequestListener
@@ -17,19 +16,18 @@ class SvgSoftwareLayerSetter : RequestListener<PictureDrawable> {
     override fun onLoadFailed(
         e: GlideException?,
         model: Any?,
-        target: Target<PictureDrawable>?,
+        target: Target<PictureDrawable>,
         isFirstResource: Boolean
     ): Boolean = false
 
     override fun onResourceReady(
         resource: PictureDrawable,
         model: Any?,
-        target: Target<PictureDrawable>?,
+        target: Target<PictureDrawable>,
         dataSource: DataSource,
         isFirstResource: Boolean
     ): Boolean {
-        val view = (target as? ImageViewTarget<*>)?.view as? ImageView
-        view?.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        (target as? ImageViewTarget<*>)?.view?.setLayerType(View.LAYER_TYPE_SOFTWARE, null)
         return false
     }
 }
