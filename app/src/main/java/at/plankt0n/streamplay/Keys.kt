1e84d603dedeaed7078c2c7bc9a7b6961eb91c15
S. J. Plankton <97785891+Planqton@users.noreply.github.com>
2025-09-07 08:31:45 +0200

    feat: add live resume option

--- app/src/main/java/at/plankt0n/streamplay/Keys.kt
+++ app/src/main/java/at/plankt0n/streamplay/Keys.kt
@@ -21,6 +21,7 @@ object Keys {
     const val PREF_AUDIO_FOCUS_MODE = "audio_focus_mode"
     const val PREF_COVER_ANIMATION_STYLE = "cover_animation_style"
     const val PREF_SCREEN_ORIENTATION = "screen_orientation"
+    const val PREF_RESUME_LIVE_AFTER_PAUSE = "resume_live_after_pause"
 
     const val PREF_PERSONAL_SYNC_URL = "personal_sync_url"
     const val DEFAULT_PERSONAL_SYNC_URL = "https://github.com/Planqton/streamplay/blob/main/teststations.json"
