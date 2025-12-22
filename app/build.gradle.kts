db79acfa9bc3fb22f6de468e72c85d8054be9bb6
Planqton <planqton@users.noreply.github.com>
2025-12-22 01:46:36 +0100

    modifi to streamplayapi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "at.plankt0n.streamplay"
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.1.1"
+        versionCode = 2
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
